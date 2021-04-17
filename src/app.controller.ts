c296038a7cdc3420f6ab269fbf073ed0fe04d0e5
g <test7even@gmail.com>
2021-04-17 12:28:37 +1200

    started adding auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  NotImplementedException,
+  Post,
+  Session,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +17,31 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('login')
+  async login(@Body() loginInput: AuthInput, @Session() session) {
+    console.log(loginInput);
+    throw new NotImplementedException();
+
+    // const user = await this.appService.getUserByEmail(loginInput.email);
+    // if (!user) {
+    //   throw new BadRequestException({
+    //     message: 'invalid login credentials',
+    //   });
+    // }
+
+    // console.log(session);
+    // session.user = user.email;
+    // return true;
+  }
+
+  @Post('register')
+  async register(@Body() registerInput: AuthInput) {
+    return this.appService.registerUser(
+      registerInput.email,
+      registerInput.password,
+    );
+  }
 }
+
+export type AuthInput = { email: string; password: string };
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
+type UserInfo = { id: number; email: string; password: string };
+type UserInfoMap = Map<string, UserInfo>;
+
+const dummyUsers: UserInfo[] = [
+  {
+    id: 1,
+    email: 't@t.com',
+    password: '1234',
+  },
+];
+
 @Injectable()
 export class AppService {
-  constructor() {}
+  private users: UserInfoMap = new Map<string, UserInfo>();
+
+  constructor() {
+    dummyUsers.forEach((u) => this.users.set(u.email, u));
+  }
 
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async registerUser(email: string, password: string) {
+    this.users.set(email, { id: Date.now(), email, password });
+    return true;
+  }
+
+  async getUserByEmail(email: string): Promise<UserInfo | null> {
+    if (!this.users.has(email)) {
+      return null;
+    }
+    const user = this.users.get(email);
+    return { ...user };
   }
 }
